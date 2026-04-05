a636f002eb28a6285359a9be5673ad588723aeab
hafazumarfaruk-svg <hafazumarfaruk@gmail.com>
2026-04-05 16:39:33 +0600

    Update App.js

--- App.js
+++ App.js
@@ -3,11 +3,11 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 // আপনার তৈরি করা সব স্ক্রিন ইমপোর্ট করা হলো
-import HomeScreen from './screens/HomeScreen';
-import PlayerScreen from './screens/PlayerScreen';
-import ChannelScreen from './screens/ChannelScreen';
-import PlaylistScreen from './screens/PlaylistPage';
-import ShortsScreen from './screens/ShortsScreen';
+import HomeScreen from './Screens/HomeScreen';
+import PlayerScreen from './Screens/PlayerScreen';
+import ChannelScreen from './Screens/ChannelScreen';
+import PlaylistScreen from './Screens/PlaylistPage';
+import ShortsScreen from './Screens/ShortsScreen';
 import HistoryPage from './Settings/HistoryPage'; 
 import SubscriptionsScreen from './Screens/SubscriptionsScreen'; 
 import SearchSettingScreen from './Settings/searchsetting'; 
